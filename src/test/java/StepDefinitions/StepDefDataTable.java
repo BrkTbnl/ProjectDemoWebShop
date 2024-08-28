8c5beb35fafbec701019f438e026329e807db06d
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-28 22:08:49 +0200

    Step def for enhanced feature file

---
+++ src/test/java/StepDefinitions/StepDefDataTable.java
@@ -0,0 +1,53 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Pages.TopNavigation;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class StepDefDataTable {
+
+    TopNavigation tn = new TopNavigation();
+    DialogContent dc = new DialogContent();
+
+    @And("Click on element in TopNavigation")
+    public void clickOnElementInTopNavigation(DataTable links) {
+        List<String> linksList = links.asList(String.class);
+
+        for (int i = 0; i < linksList.size(); i++) {
+            String link = linksList.get(i);
+            WebElement webElement = tn.getWebElement(link);
+            tn.myJSClick(webElement);
+
+        }
+    }
+
+    @When("User sends the keys in DialogContent")
+    public void userSendsTheKeysInDialogContent(DataTable dataTable) {
+        List<List<String>> items = dataTable.asLists(String.class);
+
+        for (int i = 0; i < items.size(); i++) {
+            List<String> item = items.get(i);
+            WebElement webElement = dc.getWebElement(item.get(0));
+            String text = item.get(1);
+            dc.mySendKeys(webElement,text);
+
+        }
+    }
+
+    @And("Click on element in DialogContent")
+    public void clickOnElementInDialogContent(DataTable dataTable) {
+        List<String> linksList = dataTable.asList(String.class);
+
+        for (int i = 0; i < linksList.size(); i++) {
+            String link = linksList.get(i);
+            WebElement webElement = dc.getWebElement(link);
+            dc.myJSClick(webElement);
+
+        }
+    }
+}
